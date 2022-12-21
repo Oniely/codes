7a4dafb29dc941f1631090b49fb60502851101a1
Oniely <93852403+Oniely@users.noreply.github.com>
2022-12-22 06:11:46 +0800

    Add files via upload

--- PyQt/main.py
+++ PyQt/main.py
@@ -18,7 +18,7 @@ class ADMINSIGNUP(QMainWindow):
         widget.setWindowTitle("Admin Sign Up")
 
         self.label = self.findChild(QLabel, "label_3")
-        pixmap = QPixmap("sc.png")
+        pixmap = QPixmap("sc-bg.png")
         self.label.setPixmap(pixmap)
 
         self.pushButton2 = self.findChild(QPushButton, "pushbutton_2")
@@ -130,7 +130,7 @@ class ADMINSIGNIN(QMainWindow):
         # Assigning Widget
 
         self.label = self.findChild(QLabel, "label_3")
-        pixmap = QPixmap("sc.png")
+        pixmap = QPixmap("sc-bg.png")
         self.label.setPixmap(pixmap)
 
         self.pushButton2 = self.findChild(QPushButton, "pushButton_2")
@@ -160,7 +160,7 @@ class ADMINSIGNIN(QMainWindow):
         widget.setCurrentIndex(widget.currentIndex() + 1)
 
     def mainMenu(self):
-        w_main = MainMENU()
+        w_main = ADMINHOME()
         widget.addWidget(w_main)
         widget.setCurrentIndex(widget.currentIndex() + 1)
 
@@ -216,13 +216,7 @@ class ADMINHOME(QMainWindow):
         self.home.setIcon(pixmap)
 
         self.list = self.findChild(QPushButton, "list")
-        pixmap2 = QIcon("calendar.png")
-        self.list.setIcon(pixmap2)
         self.list.clicked.connect(self.openList)
-
-        self.scfest = self.findChild(QLabel, "label")
-        pixmap4 = QPixmap("Southland Fest.png")
-        self.scfest.setPixmap(pixmap4)
 
         self.logo = self.findChild(QLabel, "label_2")
         pixmap5 = QPixmap("sc.png")
@@ -234,14 +228,37 @@ class ADMINHOME(QMainWindow):
         self.signout.clicked.connect(self.sign_in)
 
         self.secsaCount = self.findChild(QLabel, "secsaCount")
+        self.shtmCount = self.findChild(QLabel, "count")
+        self.seasCount = self.findChild(QLabel, "count_2")
+        self.sbaCount = self.findChild(QLabel, "count_3")
+        self.outCount = self.findChild(QLabel, "count_4")
+        self.totalCount = self.findChild(QLabel, "count_5")
 
+        self.total()
+        self.countOUTSIDER()
+        self.countSBA()
+        self.countSEAS()
+        self.countSHTM()
         self.loadData()
 
         # Show UI
         self.show()
 
+    def total(self):
+        conn = sqlite3.connect("southlandDB.db")
+        cursor = conn.cursor()
+
+        result = cursor.execute("SELECT * FROM customer;")
+
+        self.x = 0
+        for i in result.fetchall():
+            self.x += 1
+
+        count = str(self.x)
+        self.totalCount.setText(count)
+
     def sign_in(self):
-        w_signin = SIGNIN()
+        w_signin = ADMINSIGNIN()
         widget.addWidget(w_signin)
         widget.setCurrentIndex(widget.currentIndex() + 1)
 
@@ -249,6 +266,94 @@ class ADMINHOME(QMainWindow):
         w_list = LIST()
         widget.addWidget(w_list)
         widget.setCurrentIndex(widget.currentIndex() + 1)
+
+    def countOUTSIDER(self):
+        conn = sqlite3.connect("southlandDB.db")
+        cursor = conn.cursor()
+
+        query = """
+            select * from customer
+where department in (
+select department from customer WHERE department="Outsider"
+group by department
+having count(department) > 0
+)
+        """
+
+        rs = cursor.execute(query)
+        self.x = 0
+
+        for i in rs.fetchall():
+            self.x += 1
+
+        count = str(self.x)
+        self.outCount.setText(count)
+
+    def countSBA(self):
+        conn = sqlite3.connect("southlandDB.db")
+        cursor = conn.cursor()
+
+        query = """
+            select * from customer
+where department in (
+select department from customer WHERE department="SBA"
+group by department
+having count(department) > 0
+)
+        """
+
+        rs = cursor.execute(query)
+        self.x = 0
+
+        for i in rs.fetchall():
+            self.x += 1
+
+        count = str(self.x)
+        self.seasCount.setText(count)
+
+    def countSEAS(self):
+        conn = sqlite3.connect("southlandDB.db")
+        cursor = conn.cursor()
+
+        query = """
+            select * from customer
+where department in (
+select department from customer WHERE department="SEAS"
+group by department
+having count(department) > 0
+)
+        """
+
+        rs = cursor.execute(query)
+        self.x = 0
+
+        for i in rs.fetchall():
+            self.x += 1
+
+        count = str(self.x)
+        self.sbaCount.setText(count)
+
+    def countSHTM(self):
+        conn = sqlite3.connect("southlandDB.db")
+        cursor = conn.cursor()
+
+        query = """
+            select * from customer
+where department in (
+select department from customer WHERE department="SHTM"
+group by department
+having count(department) > 0
+)
+        """
+
+        rs = cursor.execute(query)
+        self.x = 0
+
+        for i in rs.fetchall():
+            self.x += 1
+
+        count = str(self.x)
+        self.shtmCount.setText(count)
 
     def loadData(self):
         conn = sqlite3.connect("southlandDB.db")
@@ -288,13 +393,21 @@ class LIST(QMainWindow):
         self.home.clicked.connect(self.openHome)
 
         self.schedule = self.findChild(QPushButton, "list")
-        pixmap2 = QIcon("calendar.png")
-        self.schedule.setIcon(pixmap2)
 
         self.signout = self.findChild(QPushButton, "signout")
         pixmap4 = QIcon("signout.png")
         self.signout.setIcon(pixmap4)
         self.signout.clicked.connect(self.sign_in)
+
+        self.showbtn = self.findChild(QPushButton, "showbtn")
+        self.showbtn.clicked.connect(self.loadData)
+
+        self.lineSearch = self.findChild(QLineEdit, "lineSearch")
+        self.searchbtn = self.findChild(QPushButton, "searchbtn")
+        self.searchbtn.clicked.connect(self.searchData)
+
+        self.delbtn = self.findChild(QPushButton, "delbtn")
+        self.delbtn.clicked.connect(self.deleteData)
 
         self.tableData = self.findChild(QTableWidget, "tableData")
         self.tableData.setHorizontalHeaderLabels(
@@ -303,6 +416,48 @@ class LIST(QMainWindow):
         self.loadData()
 
         self.show()
+
+    def deleteData(self):
+        conn = sqlite3.connect("southlandDB.db")
+        cursor = conn.cursor()
+
+        query = "SELECT * FROM customer"
+        res = cursor.execute(query)
+
+        for row in enumerate(res):
+            if row[0] == self.tableData.currentRow():
+                data = row[1]
+                id = data[0]
+                name = data[1]
+                tkNum = data[2]
+
+                cursor.execute("DELETE FROM customer WHERE id=? AND name=? AND tkNum=?", [
+                               id, name, tkNum])
+                conn.commit()
+                self.loadData()
+
+    def searchData(self):
+        conn = sqlite3.connect("southlandDB.db")
+        cursor = conn.cursor()
+
+        query = """select * from customer
+                    where name in (
+                    select name from customer WHERE name=?
+                    group by name
+                    having count(id) > 0
+                )"""
+
+        self.tableData.setRowCount(0)
+
+        result = cursor.execute(query, [self.lineSearch.text()])
+
+        for row_number, row_data in enumerate(result):
+            self.tableData.insertRow(row_number)
+            for column_number, data in enumerate(row_data):
+                self.tableData.setItem(
+                    row_number, column_number, QTableWidgetItem(str(data)))
+
+        self.lineSearch.clear()
 
     def loadData(self):
         conn = sqlite3.connect("southlandDB.db")
@@ -319,7 +474,7 @@ class LIST(QMainWindow):
                     row_number, column_number, QTableWidgetItem(str(data)))
 
     def sign_in(self):
-        w_signin = SIGNIN()
+        w_signin = ADMINSIGNIN()
         widget.addWidget(w_signin)
         widget.setCurrentIndex(widget.currentIndex() + 1)
 
@@ -343,7 +498,7 @@ class SIGNIN(QMainWindow):
         # Assigning Widget
 
         self.label = self.findChild(QLabel, "label_3")
-        pixmap = QPixmap("sc.png")
+        pixmap = QPixmap("sc-bg.png")
         self.label.setPixmap(pixmap)
 
         self.pushButton2 = self.findChild(QPushButton, "pushButton_2")
@@ -374,7 +529,7 @@ class SIGNIN(QMainWindow):
         widget.setCurrentIndex(widget.currentIndex() + 1)
 
     def mainMenu(self):
-        w_main = MainMENU()
+        w_main = HOME()
         widget.addWidget(w_main)
         widget.setCurrentIndex(widget.currentIndex() + 1)
 
@@ -419,7 +574,7 @@ class SIGNUP(QMainWindow):
         widget.setWindowTitle("Sign Up")
         # Assigning Widget
         self.label = self.findChild(QLabel, "label_3")
-        pixmap = QPixmap("sc.png")
+        pixmap = QPixmap("sc-bg.png")
         self.label.setPixmap(pixmap)
 
         self.pushButton2 = self.findChild(QPushButton, "pushButton_2")
@@ -434,15 +589,15 @@ class SIGNUP(QMainWindow):
         self.menubar = self.findChild(QMenuBar, "menubar")
         self.menu = self.findChild(QMenu, "menu")
         self.actionAdmin = self.findChild(QAction, "admin")
-        self.actionAdmin.triggered.connect(self.adminSignIn)
+        self.actionAdmin.triggered.connect(self.adminSignUp)
 
         # Show UI
 
         # Show UI
         self.show()
 
-    def adminSignIn(self):
-        w_asignin = ADMINSIGNIN()
+    def adminSignUp(self):
+        w_asignin = ADMINSIGNUP()
         widget.addWidget(w_asignin)
         widget.setCurrentIndex(widget.currentIndex() + 1)
 
@@ -510,11 +665,6 @@ class MainMENU(QMainWindow):
         self.home.setIcon(pixmap)
         self.home.clicked.connect(self.openHome)
 
-        self.schedule = self.findChild(QPushButton, "calendar")
-        pixmap2 = QIcon("calendar.png")
-        self.schedule.setIcon(pixmap2)
-        self.schedule.clicked.connect(self.openSched)
-
         self.add = self.findChild(QPushButton, "add")
         pixmap3 = QIcon("add (1).png")
         self.add.setIcon(pixmap3)
@@ -562,11 +712,6 @@ class MainMENU(QMainWindow):
     def openHome(self):
         w_home = HOME()
         widget.addWidget(w_home)
-        widget.setCurrentIndex(widget.currentIndex() + 1)
-
-    def openSched(self):
-        w_sched = SCHED()
-        widget.addWidget(w_sched)
         widget.setCurrentIndex(widget.currentIndex() + 1)
 
     def comboBox(self):
@@ -737,19 +882,10 @@ class HOME(QMainWindow):
         pixmap = QIcon("home.png")
         self.home.setIcon(pixmap)
 
-        self.schedule = self.findChild(QPushButton, "calendar")
-        pixmap2 = QIcon("calendar.png")
-        self.schedule.setIcon(pixmap2)
-        self.schedule.clicked.connect(self.openSched)
-
         self.add = self.findChild(QPushButton, "add")
         pixmap3 = QIcon("add (1).png")
         self.add.setIcon(pixmap3)
         self.add.clicked.connect(self.openAdd)
-
-        self.scfest = self.findChild(QLabel, "label")
-        pixmap4 = QPixmap("Southland Fest.png")
-        self.scfest.setPixmap(pixmap4)
 
         self.logo = self.findChild(QLabel, "label_2")
         pixmap5 = QPixmap("sc.png")
@@ -762,62 +898,6 @@ class HOME(QMainWindow):
 
         # Show UI
         self.show()
-
-    def openAdd(self):
-        w_add = MainMENU()
-        widget.addWidget(w_add)
-        widget.setCurrentIndex(widget.currentIndex() + 1)
-
-    def sign_in(self):
-        w_signin = SIGNIN()
-        widget.addWidget(w_signin)
-        widget.setCurrentIndex(widget.currentIndex() + 1)
-
-    def openSched(self):
-        w_sched = SCHED()
-        widget.addWidget(w_sched)
-        widget.setCurrentIndex(widget.currentIndex() + 1)
-
-
-class SCHED(QMainWindow):
-    def __init__(self):
-        super(SCHED, self).__init__()
-
-        # Opening Ui File
-
-        uic.loadUi("schedule.ui", self)
-        widget.setFixedWidth(1000)
-        widget.setFixedHeight(600)
-
-        widget.setWindowTitle("Schedule")
-
-        # Widgets
-
-        self.home = self.findChild(QPushButton, "home")
-        pixmap = QIcon("home.png")
-        self.home.setIcon(pixmap)
-        self.home.clicked.connect(self.openHome)
-
-        self.schedule = self.findChild(QPushButton, "calendar")
-        pixmap2 = QIcon("calendar.png")
-        self.schedule.setIcon(pixmap2)
-
-        self.add = self.findChild(QPushButton, "add")
-        pixmap3 = QIcon("add (1).png")
-        self.add.setIcon(pixmap3)
-        self.add.clicked.connect(self.openAdd)
-
-        self.signout = self.findChild(QPushButton, "signout")
-        pixmap4 = QIcon("signout.png")
-        self.signout.setIcon(pixmap4)
-        self.signout.clicked.connect(self.sign_in)
-
-        self.show()
-
-    def openHome(self):
-        w_home = HOME()
-        widget.addWidget(w_home)
-        widget.setCurrentIndex(widget.currentIndex() + 1)
 
     def openAdd(self):
         w_add = MainMENU()
